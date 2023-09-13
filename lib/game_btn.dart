7283ee2ddfd54d4391f1b42df7172b6e979202ff
Hany <hany@hanykhodair.com>
2023-09-14 00:44:53 +0300

    Game_Btn Class

--- lib/game_btn.dart
+++ lib/game_btn.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 
 class GameBtn extends StatelessWidget {
   String txt;
-  GameBtn(this.txt);
+  Function onGameClick;
+  int index;
+
+  GameBtn(this.txt, this.onGameClick, this.index);
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ElevatedButton(onPressed:() {
-
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.indigo,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+            side: BorderSide(
+                color: Colors.white, width: 2)
+          )
+        ),
+        onPressed:() {
+        onGameClick(index);
       }, child: Text(txt, style: TextStyle(fontSize: 55,
       fontWeight: FontWeight.w100),),),
     );
--- lib/xo.dart
+++ lib/xo.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/game_btn.dart';
 
-class XOGame extends StatelessWidget {
+class XOGame extends StatefulWidget {
+
+
+  @override
+  State<XOGame> createState() => _XOGameState();
+}
+
+class _XOGameState extends State<XOGame> {
+  List<String>board=[
+    "","","",
+    "","","",
+    "","",""
+  ];
+
+  int score1=0;
+  int score2 = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +31,20 @@ class XOGame extends StatelessWidget {
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text('Player 1'),
-                    Text('0'),
+                    Text('Player 1',
+                    style: TextStyle(fontSize: 35),),
+                    Text('$score1', style: TextStyle(
+                        fontSize:35 ),),
                   ],
                 ),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text('Player 2'),
-                    Text('0'),
+                    Text('Player 2', style: TextStyle(
+                      fontSize: 35
+                    ),),
+                    Text('$score2',
+                      style: TextStyle(fontSize: 35),),
                   ],
                 )
               ],
@@ -34,9 +54,9 @@ class XOGame extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                GameBtn('x'),
-                GameBtn('O'),
-                GameBtn('X'),
+                GameBtn(board[0],onBtnClick,0),
+                GameBtn(board[1],onBtnClick,1),
+                GameBtn(board[2],onBtnClick,2),
               ],
             ),
           ),
@@ -44,9 +64,9 @@ class XOGame extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                GameBtn('x'),
-                GameBtn('O'),
-                GameBtn('X'),
+                GameBtn(board[3],onBtnClick,3),
+                GameBtn(board[4],onBtnClick,4),
+                GameBtn(board[5],onBtnClick,5),
               ],
             ),
           ),
@@ -54,13 +74,78 @@ class XOGame extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                GameBtn('x'),
-                GameBtn('O'),
-                GameBtn('X'),
+                GameBtn(board[6],onBtnClick,6),
+                GameBtn(board[7],onBtnClick,7),
+                GameBtn(board[8],onBtnClick,8),
               ],
             ),
           ),        ],
       ),
     );
+  }
+
+  int counter = 1;
+
+  onBtnClick(index){
+    if (board[index].isNotEmpty) return;
+    if (counter.isOdd){
+      board[index]='X';
+      score1++;
+      bool win = checkWinner('X');
+      if(win){
+        score1+=10;
+        resetBoard();
+      }
+    }
+    else{
+      board[index]='O';
+      score2++;
+      bool win = checkWinner('O');
+      if (win){
+        score2+=10;
+        resetBoard();
+      }
+    }
+    if (counter ==9){
+      resetBoard();
+    }
+    setState(() {
+      counter++;
+    });
+
+  }
+  checkWinner(String symbol){
+
+    for(int i =0; i<9; i+=3){
+      if(board[i]==symbol && board[i+1]==symbol && board[i+2]==symbol){
+        return true;
+      }
+    }
+
+    for(int i=0; i<3; i++){
+      if(board[i]==symbol && board[i+3]==symbol && board[i+6]==symbol){
+        return true;
+      }
+    }
+
+    if(board[0]==symbol && board[4]==symbol && board[8]==symbol)
+      {
+        return true;
+      }
+    if(board[2]==symbol && board[4]==symbol && board[6]==symbol)
+      {
+        return true;
+      }
+    return false;
+  }
+
+  resetBoard()
+  {
+    board=[
+      "","","",
+      "","","",
+      "","",""
+    ];
+    counter =0;
   }
 }
